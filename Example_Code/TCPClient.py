51475492fd6bbb659ec1c472e89e3c9be4d31b48
Alex Jones <alextigernick@gmail.com>
2019-02-20 09:09:26 -0600

    IT works!

--- Example_Code/TCPClient.py
+++ Example_Code/TCPClient.py
@@ -1,41 +1,66 @@
-from socket import *
+import socket
 import cv2
 import time
 import sys
 import numpy
+import struct,os
 
 IP = '127.0.0.1'
 PORT = 5005
-
+TCP_IP = '127.0.0.1'
 cap = cv2.VideoCapture(0)
-socket = socket(AF_INET,SOCK_STREAM)
-socket.connect((IP,PORT))
+send = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, PORT+1))
+send.connect((IP,PORT))
+s.listen(1)
 #res = cam.getImage().size
 #socket.send(str(res[0])) #Send webcam resolution
 
 time.sleep(1)
-
-while True:
-	ret, frame = cap.read()
-	if ret:
-		encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),100]
-		result, imgencode = cv2.imencode('.jpg', frame, encode_param)
-		stringData = imgencode.tostring()
-		try:
-			time.sleep(.0001) #otherwise send to fast and the server receive images in pieces and fail
-			socket.send( str(len(stringData)).ljust(16))
-			socket.send(stringData)
-		except KeyboardInterrupt:
-			socket.send("quit")
-			socket.close()
-			break
-		except Exception:
-			print("Error from server side")
-			socket.close()
-			break
-		decimg=cv2.imdecode(imgencode,1)
-		cv2.imshow('CLIENT',decimg)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+try:
+	conn, addr = s.accept()
+	conn.setblocking(False)
+	while True:
+		ret, frame = cap.read()
+		if ret:
+			encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),100]
+			result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+			stringData = imgencode.tostring()
+			try:
+				#time.sleep(.0001) #otherwise send to fast and the server receive images in pieces and fail
+				send.send( str(len(stringData)).ljust(16))
+				send.send(stringData)
+			except KeyboardInterrupt:
+				send.send("quit")
+				send.close()
+				break
+			except Exception:
+				print("Error from server side")
+				send.close()
+				break
+			decimg=cv2.imdecode(imgencode,1)
+			try:
+				rec = conn.recv(8)
+				(x,y) = struct.unpack("ii",rec)
+				print "Hand at x:",
+				print x,
+				print " y:",
+				print y
+				cv2.circle(decimg,(x,y),5,(255,255,0),3)
+			except socket.error:
+				pass
+			cv2.imshow('CLIENT',decimg)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+except Exception as e:
+	exc_type, exc_obj, exc_tb = sys.exc_info()
+	fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+	print(exc_type, fname, exc_tb.tb_lineno)
+	print(e)
+s.close()
+send.close()
 cap.release()
 cv2.destroyAllWindows()
+conn.close()
--- Example_Code/TCPServer.py
+++ Example_Code/TCPServer.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
- 
+import struct
 import socket
 import select
 import numpy
 import cv2
+import sys,os
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 def detectFaces(frame):
     frameCenterX = ((frame.shape[1] / 2))
     frameCenterY = ((frame.shape[0] / 2))
-            
+    retHands = []
             # Draw Statics
     font = cv2.FONT_HERSHEY_SIMPLEX
     circle = cv2.circle(frame,(frameCenterX,frameCenterY), 2, (0,0,255), 1)    #Center Target
@@ -22,7 +23,6 @@ def detectFaces(frame):
     faces = face_cascade.detectMultiScale(gray, 1.1, 5) #Detect faces
     for(x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2) #Display box for viewer  
-        print str(len(faces)) + " face(s) found"
         if (len(faces)== 1): #Detected 1 face and in standalone mode
             Pt1X = x-w #Tuned constants for rough area of arm location based on size of face 
             if Pt1X < 20: #Must be positive
@@ -40,7 +40,8 @@ def detectFaces(frame):
             cropped = frame[Pt2Y:Pt1Y, Pt2X:Pt1X]   #Crop image to expected area where hand would be   
             if cropped.size >= 0:                                    
                 grey_cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
-                hands = hand_cascade.detectMultiScale(grey_cropped, 1.1, 5)      
+                hands = hand_cascade.detectMultiScale(grey_cropped, 1.1, 5) 
+                retHands = hands     
                 for (x,y,w,h) in hands:
                     cv2.rectangle(frame,(x+Pt2X,y+Pt2Y),(x+w+Pt2X,y+h+Pt2Y),(0,0,255),2)
                     if(len(hands) == 1): #1 hand found, draw target, vector and turn towards target
@@ -57,6 +58,7 @@ def detectFaces(frame):
                         cv2.putText(frame,str(distanceY),(frameCenterX-10,frameCenterY-distanceY), font, .5,(255,255,255),2,cv2.LINE_AA)
                         vector = cv2.arrowedLine(frame, (frameCenterX, frameCenterY), (targetCenterX, targetCenterY), (0,0,255), 1)
                         vectorX = cv2.arrowedLine(frame, (frameCenterX, frameCenterY), (targetCenterX, frameCenterY), (0,255,0), 1)                            #vectorY = cv2.arrowedLine(frame, (frameCenterX, frameCenterY), (frameCenterX, targetCenterY), (0,255,0), 1)  
+    return retHands
 
 def recvall(sock, count):
     buf = b''
@@ -79,25 +81,37 @@ def getLatestFrame(conn):
             stringData = recvall(conn, int(length))       
         elif length != -1:
             return stringData
-    
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-while 1:
-    conn, addr = s.accept()
-    print 'Connection address:', addr
-    conn.setblocking(False)
+send = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+try:
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)
     while 1:
+        conn, addr = s.accept()
+        print 'Connection address:', addr
+        send.connect((addr[0],TCP_PORT+1))
         
-        stringData = getLatestFrame(conn)
-        
-        data = numpy.fromstring(stringData, dtype='uint8')
-        decimg=cv2.imdecode(data,1)
-        detectFaces(decimg)
-        cv2.imshow('SERVER',decimg)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    conn.close()
-cap.release()
+        conn.setblocking(False)
+        while 1:
+            stringData = getLatestFrame(conn)
+            data = numpy.fromstring(stringData, dtype='uint8')
+            decimg=cv2.imdecode(data,1)
+            hands = detectFaces(decimg)
+            if len(hands):
+                print(hands)
+                (x,y,w,h) = hands[0]
+                send.send(struct.pack("ii",x+w/2,y+h/2))
+            cv2.imshow('SERVER',decimg)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        send.close()
+        conn.close()
+except Exception as e:
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    print(exc_type, fname, exc_tb.tb_lineno)
+    print(e)
+s.close()
+send.close()
 cv2.destroyAllWindows()
+conn.close()
